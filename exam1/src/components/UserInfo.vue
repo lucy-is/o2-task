8f933182b912d80ffc6ca89874a306f929db259a
chanCo <loinsective@naver.com>
2022-10-25 14:17:32 +0900

    1차과제/response

--- exam1/src/components/UserInfo.vue
+++ exam1/src/components/UserInfo.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h2>사용자정보</h2>
-    <table border="1">
+    <table>
       <thead>
         <tr>
           <th v-for="(infoTitle, i) in infoTitles" :key="i">{{ infoTitle }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="user in users" :key="user.id">
+        <tr v-for="user in userData" :key="user.id">
           <td>{{ user.id }}</td>
           <td>{{ user.name }}</td>
           <td>{{ user.username }}</td>
@@ -17,38 +17,53 @@
       </tbody>
       <tfoot>
         <tr>
-          <td>pagination</td>
+          <td colspan="4" v-if="userData.length > 1">pagination</td>
+          <td colspan="4" v-else-if="userData.length === 0">
+            조회할 데이터가 없습니다.
+          </td>
         </tr>
       </tfoot>
     </table>
   </div>
+  <!-- <p>{{ userData }}</p> -->
 </template>
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
   name: 'UserInfo',
+  props: {
+    userData: Array
+  },
   data() {
     return {
       infoTitles: ['#', '성명', '닉네임', '회사명'],
       users: []
     }
   },
-  methods: {
-    response() {
-      axios
-        .get('https://jsonplaceholder.typicode.com/users')
-        .then((res) => {
-          this.users = res.data
-        })
-        .catch((err) => console.log(err))
-    },
-    search() {}
-  },
-  setup() {},
-  created() {
-    this.response()
-  }
+  methods: {},
+  setup() {}
 }
 </script>
-<style></style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+
+table {
+  width: 100%;
+  text-align: center;
+  border-collapse: collapse;
+}
+
+th {
+  padding: 10px;
+  border: 1px solid #222;
+  background-color: #e5e5e5;
+}
+
+td {
+  padding: 10px;
+  border: 1px solid #222;
+}
+</style>
--- exam1/src/views/UserManageView.vue
+++ exam1/src/views/UserManageView.vue
@@ -5,23 +5,25 @@
 
     <ToHomeVue />
 
-    <form class="search-wrap">
+    <form class="search-wrap" @submit="search">
       <input
         class="search"
         type="text"
         placeholder="닉네임"
         v-model="searchUser"
       />
-      <button class="search-btn">검색</button>
+      <button type="submit" class="search-btn">검색</button>
+      <p class="errorMsg">{{ errorMsg }}</p>
     </form>
-    <p>{{ searchUser }}</p>
 
-    <UserInfoVue />
+    <UserInfoVue :userData="userData" />
   </div>
 </template>
 
 <!-- TODO: script -->
 <script>
+import axios from 'axios'
+
 import ToHomeVue from '@/components/ToHome.vue'
 import UserInfoVue from '@/components/UserInfo.vue'
 
@@ -33,8 +35,51 @@ export default {
   },
   data() {
     return {
-      searchUser: ''
+      searchUser: '',
+      errorMsg: '',
+      userData: []
     }
+  },
+  methods: {
+    response() {
+      const response = async () => {
+        try {
+          const item = await axios.get(
+            'https://jsonplaceholder.typicode.com/users'
+          )
+          this.userData = item.data
+        } catch (err) {
+          console.log(err)
+        }
+      }
+      response()
+    },
+    search(e) {
+      e.preventDefault()
+
+      if (this.searchUser === '') {
+        this.errorMsg = '성명을 입력해주세요.'
+        setTimeout(() => {
+          this.errorMsg = ''
+        }, 2000)
+        return false
+      } else {
+        const searchData = async () => {
+          try {
+            const item = await axios.get(
+              `https://jsonplaceholder.typicode.com/users?username=${this.searchUser}`
+            )
+            this.userData = item.data
+          } catch (err) {
+            console.log(err)
+          }
+        }
+        searchData()
+      }
+    }
+  },
+  mounted() {
+    this.response()
   }
 }
 </script>
@@ -74,5 +119,11 @@ h1 {
 
 .search-btn:active {
   transform: scale(0.9, 0.9);
+}
+
+.errorMsg {
+  position: absolute;
+  color: red;
+  padding-top: 10px;
 }
 </style>
